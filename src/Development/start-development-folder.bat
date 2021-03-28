c1e32ea47ae804965fd41c9ae13f8f58d5075035
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-03-28 18:30:20 -0300

    news

--- src/Development/start-development-folder.bat
+++ src/Development/start-development-folder.bat
@@ -35,8 +35,8 @@ rd /s /q %DEVELOPMENT_HOME%\scripts\windows
 
 xcopy /y /s scripts\windows\* %DEVELOPMENT_HOME%\scripts\
 
-del /s /q %DEVELOPMENT_HOME%\start-development-folder.bat
-del /s /q %DEVELOPMENT_HOME%\start-development-folder.sh
+rem del /s /q %DEVELOPMENT_HOME%\start-development-folder.bat
+rem del /s /q %DEVELOPMENT_HOME%\start-development-folder.sh
 
 rem current dir's backup
 set back=%cd%
@@ -52,12 +52,14 @@ for /d %%d in (*) do (
        cd %%d
     
        for %%f in (*.bat) do (   
-           echo call %DEVELOPMENT_HOME%\scripts\%%d\%%f > %DEVELOPMENT_HOME%\scripts\%%f       
+           @echo echo call %DEVELOPMENT_HOME%\scripts\%%d\%%f > %DEVELOPMENT_HOME%\scripts\%%f 
        )
        
        cd ..
     )
 )
+
+SETX PATH "%PATH%;%DEVELOPMENT_HOME%\scripts\"
 
 rem go back 
 cd %back%
