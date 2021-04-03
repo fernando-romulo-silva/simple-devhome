5e1ce6f6f05f534a4b7602e7e190b4b70f989352
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-04-03 17:41:42 -0300

    news

---
+++ src/Development/scripts/linux/payara/set-payara-5.2-full.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# go to script dir
+back_wild=$(pwd)
+cd $DEVELOPMENT_HOME/scripts/wildfly
+
+echo "=============================================================================================================================="
+echo "Set the environment for Payara 5.2 Full Profile"
+
+# -----------------------------------------------------------------------------------------------------
+# check the DEVELOPMENT_HOME variable
+result=$(../internal/check-develpment-folder.sh)
+if [ -z "${result##*error*}" ] ; then
+  echo $result
+  exit 1
+else
+  echo $result
+fi
+
+# -----------------------------------------------------------------------------------------------------
+# install java 11
+. $DEVELOPMENT_HOME/scripts/java/set-java-11-zulu.sh
+
+# -----------------------------------------------------------------------------------------------------
+# install Payara 5.2
+. $DEVELOPMENT_HOME/scripts/internal/set-program.sh https://s3-eu-west-1.amazonaws.com/payara.fish/Payara+Downloads/5.2021.1/payara-5.2021.1.zip payara-5.2-full servers/payara PAYARA_HOME
+
+# -----------------------------------------------------------------------------------------------------
+# doc Wildfly
+. $DEVELOPMENT_HOME/scripts/payara/doc-payara.sh
+
+# go back
+cd $back_wild
+
+echo "=============================================================================================================================="
