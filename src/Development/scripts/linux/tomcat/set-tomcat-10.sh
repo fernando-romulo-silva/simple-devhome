a9bb4a540dd2316ac82e2c37e087e15607ca0649
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2021-04-30 13:43:38 -0300

    news

--- src/Development/scripts/linux/tomcat/set-tomcat-10.sh
+++ src/Development/scripts/linux/tomcat/set-tomcat-10.sh
@@ -4,7 +4,7 @@ back_tom=$(pwd)
 cd $DEVELOPMENT_HOME/scripts/tomcat
 
 echo "=============================================================================================================================="
-echo "Set the Environment for Tomcat 9.0 (JEE 8 Web = Servlet 4.0, JSP 2.3, EL 3.0, WebSocket 1.1, Authentication (JASIC) 1.1)"
+echo "Set the Environment for Tomcat 10.0 (JEE 9 Web = Servlet 5.0, JSP 3.0, EL 4.0, WebSocket 2.0, Authentication (JASIC) 2.0)"
 
 # -----------------------------------------------------------------------------------------------------
 # check the DEVELOPMENT_HOME variable
@@ -17,12 +17,12 @@ else
 fi
 
 # -----------------------------------------------------------------------------------------------------
-# install java 8
-. $DEVELOPMENT_HOME/scripts/set-java-8-zulu.sh
+# install java 11
+. $DEVELOPMENT_HOME/scripts/set-java-11-zulu.sh
 
 # -----------------------------------------------------------------------------------------------------
-# install tomcat 9
-. $DEVELOPMENT_HOME/scripts/internal/set-program.sh https://downloads.apache.org/tomcat/tomcat-9/v9.0.45/bin/apache-tomcat-9.0.45.tar.gz apache-tomcat-9.0 servers/apache-tomcat CATALINA_HOME
+# install tomcat 10
+. $DEVELOPMENT_HOME/scripts/internal/set-program.sh https://downloads.apache.org/tomcat/tomcat-10/v10.0.5/bin/apache-tomcat-10.0.5.tar.gz apache-tomcat-10.0 servers/apache-tomcat CATALINA_HOME
 
 # -----------------------------------------------------------------------------------------------------
 # doc tomcat
