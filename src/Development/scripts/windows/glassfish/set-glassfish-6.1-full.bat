6cefc26b4b24f0894076ae6ffbe7aba86e40a334
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-10-16 09:52:13 +0100

    news

--- src/Development/scripts/windows/glassfish/set-glassfish-6.1-full.bat
+++ src/Development/scripts/windows/glassfish/set-glassfish-6.1-full.bat
@@ -17,9 +17,34 @@ if /I "%var1:error=%" neq "%var1%" (
 ) 
 
 rem -----------------------------------------------------------------------------------------------------
-rem install java 8
-call ..\java\set-java-8-zulu
+rem check JAVA_HOME
+if %JAVA_HOME%=="" (
+    echo error: JAVA_HOME is not configured, please configure it.
+    cd %back%
+    exit /B
+)
 
+rem -----------------------------------------------------------------------------------------------------
+rem check Java version
+set JAVA_VERSION=0
+for /f "tokens=3" %%g in ('java -Xms32M -Xmx32M -version 2^>^&1 ^| findstr /i "version"') do (
+  set JAVA_VERSION=%%g
+)
+
+set JAVA_VERSION=%JAVA_VERSION:"=%
+for /f "delims=.-_ tokens=1-2" %%v in ("%JAVA_VERSION%") do (
+  if /I "%%v" EQU "1" (
+    set JAVA_VERSION=%%w
+  ) else (
+    set JAVA_VERSION=%%v
+  )
+)
+
+if not %JAVA_VERSION%==11 (
+	echo error: Java 11 is required!
+	cd %back%
+    exit /B	
+)
 rem -----------------------------------------------------------------------------------------------------
 rem install glassfish 5.1 full
 call ..\internal\set-program https://download.eclipse.org/ee4j/glassfish/glassfish-6.1.0.zip glassfish-6.1-full servers/glassfish GLASSFISH_HOME     
