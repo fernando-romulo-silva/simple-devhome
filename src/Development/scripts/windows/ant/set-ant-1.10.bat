a7b5df485c8e4347498bff89a017d0651f1a1461
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-10-16 09:06:50 +0100

    news

--- src/Development/scripts/windows/ant/set-ant-1.10.bat
+++ src/Development/scripts/windows/ant/set-ant-1.10.bat
@@ -4,7 +4,7 @@ set back=%cd%
 cd %DEVELOPMENT_HOME%\scripts\ant
 
 echo ==============================================================================================================================
-echo Set the environment for Ant 1.9 (JDK 8+)
+echo Set the environment for Ant 1.10 (JDK 8+)
 
 rem -----------------------------------------------------------------------------------------------------
 rem check the DEVELOPMENT_HOME variable
@@ -12,7 +12,7 @@ call ..\internal\check-develpment-folder var1
 if /I "%var1:error=%" neq "%var1%" (
 	echo %var1%
     exit /B 
-) else (
+) else (cd
  	echo %var1%
 )
 
@@ -20,6 +20,7 @@ rem ----------------------------------------------------------------------------
 rem check JAVA_HOME
 if %JAVA_HOME%=="" (
    echo Java home, JAVA_HOME, is not configured, please configure it.
+   cd %back%
    exit /B 
 )
 
@@ -41,6 +42,7 @@ for /f "delims=.-_ tokens=1-2" %%v in ("%JAVA_VERSION%") do (
 
 if %JAVA_VERSION% LSS 8 (
 	echo Java 8 or higher is required!
+	cd %back%
     exit /B	
 ) 
 
--- src/Development/scripts/windows/ant/set-ant-1.9.bat
+++ src/Development/scripts/windows/ant/set-ant-1.9.bat
@@ -20,8 +20,31 @@ rem ----------------------------------------------------------------------------
 rem check JAVA_HOME
 if %JAVA_HOME%=="" (
     echo Java home, JAVA_HOME, is not configured, please configure it.
+    cd %back%
     exit /B
 )
+
+rem -----------------------------------------------------------------------------------------------------
+rem check Java version
+set JAVA_VERSION=0
+for /f "tokens=3" %%g in ('java -Xms32M -Xmx32M -version 2^>^&1 ^| findstr /i "version"') do (
+  set JAVA_VERSION=%%g
+)
+
+set JAVA_VERSION=%JAVA_VERSION:"=%
+for /f "delims=.-_ tokens=1-2" %%v in ("%JAVA_VERSION%") do (
+  if /I "%%v" EQU "1" (
+    set JAVA_VERSION=%%w
+  ) else (
+    set JAVA_VERSION=%%v
+  )
+)
+
+if %JAVA_VERSION% GTR 7 (
+	echo Java 7 or Java 6 is required!
+	cd %back%
+    exit /B	
+) 
 
 rem -----------------------------------------------------------------------------------------------------
 rem install ant
